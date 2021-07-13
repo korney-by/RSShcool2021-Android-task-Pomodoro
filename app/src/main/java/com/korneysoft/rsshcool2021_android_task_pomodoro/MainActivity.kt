ae7c107ff170e2503c57490b39edc9331ec4c972
korney <korney.by@gmail.com>
2021-07-13 17:22:04 +0300

    feat: interface - first stage

--- app/src/main/java/com/korneysoft/rsshcool2021_android_task_pomodoro/MainActivity.kt
+++ app/src/main/java/com/korneysoft/rsshcool2021_android_task_pomodoro/MainActivity.kt
@@ -2,10 +2,14 @@ package com.korneysoft.rsshcool2021_android_task_pomodoro
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.korneysoft.rsshcool2021_android_task_pomodoro.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
